5d62b76c61afd2a0c319dd8e57623730dc34bcee
qghop <cschatmeyer@gmail.com>
2025-07-07 23:25:15 -0400

    Transform data functions

--- src/extract.py
+++ src/extract.py
@@ -4,21 +4,23 @@ from datetime import datetime
 import os
 
 def fetch_stock_data(ticker, start, end):
-    df = yf.download(ticker, start=start, end=end, auto_adjust=True)
+    df = yf.download(ticker, start=start, end=end, auto_adjust=True, progress=False)
     if df is not None:
         df.reset_index(inplace=True)
+        df.columns = [col[0] if isinstance(col, tuple) else col for col in df.columns]
         pl_df = pl.from_pandas(df)
+        print(pl_df.columns)
         return pl_df
     else:
         raise ValueError(f"No data found for ticker: {ticker}")
 
-def save_bronze_data(ticker: str, df: pl.DataFrame):
+def save_raw_data(ticker, df):
     os.makedirs("data/bronze", exist_ok=True)
-    df.write_ipc(f"data/bronze/{ticker}.feather")
+    df.write_ipc(f"data/bronze/{ticker}_raw.feather")
 
 if __name__ == "__main__":
     ticker = "AAPL"
     start = "2020-01-01"
     end = datetime.today().strftime('%Y-%m-%d')
     df = fetch_stock_data(ticker, start, end)
-    save_bronze_data(ticker, df)
+    save_raw_data(ticker, df)
---
+++ src/transform.py
@@ -0,0 +1,18 @@
+import polars as pl
+import os
+
+def clean_raw_data(ticker):
+    df = pl.read_ipc(f"data/bronze/{ticker}_raw.feather")
+
+    df = df.with_columns([
+        (pl.col("Close") - pl.col("Open")).alias("Price_Diff"),
+        (pl.col("Close").pct_change() * 100).alias("Daily_Return"),
+        pl.col("Close").rolling_mean(window_size=20).alias("SMA_20"),
+        pl.col("Close").rolling_mean(window_size=50).alias("SMA_50")
+    ]).drop_nulls()
+
+    os.makedirs("data/silver", exist_ok=True)
+    df.write_ipc(f"data/silver/{ticker}_cleaned.feather")
+
+if __name__ == "__main__":
+    clean_raw_data("AAPL")
